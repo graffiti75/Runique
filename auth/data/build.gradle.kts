fc8048497f5918e2d4c30d871bdf2d764c52efa9
Rodrigo Cericatto <graffiti75@gmail.com>
2025-04-21 23:17:37 -0300

    Implement JvmKtorConventionPlugin (with compile error on :run:location module)

--- auth/data/build.gradle.kts
+++ auth/data/build.gradle.kts
@@ -1,35 +1,10 @@
 plugins {
-	alias(libs.plugins.android.library)
-	alias(libs.plugins.jetbrainsKotlinAndroid)
+	alias(libs.plugins.runique.android.library)
+	alias(libs.plugins.runique.jvm.ktor)
 }
 
 android {
 	namespace = "br.android.cericatto.auth.data"
-	compileSdk = 35
-
-	defaultConfig {
-		minSdk = 25
-
-		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-		consumerProguardFiles("consumer-rules.pro")
-	}
-
-	buildTypes {
-		release {
-			isMinifyEnabled = false
-			proguardFiles(
-				getDefaultProguardFile("proguard-android-optimize.txt"),
-				"proguard-rules.pro"
-			)
-		}
-	}
-	compileOptions {
-		sourceCompatibility = JavaVersion.VERSION_17
-		targetCompatibility = JavaVersion.VERSION_17
-	}
-	kotlinOptions {
-		jvmTarget = "17"
-	}
 }
 
 dependencies {
